903462fda84bbc1bf94b66f4aa34ff8c19db09d9
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-01-02 07:44:09 +0530

    Update Jan-02.cpp

--- Jan-02.cpp
+++ Jan-02.cpp
@@ -1,1 +1,26 @@
+//Link- https://www.geeksforgeeks.org/problems/largest-sum-subarray-of-size-at-least-k3121/1
 
+
+class Solution{
+    public:    
+    long long int maxSumWithK(long long int a[], long long int n, long long int k) 
+    {
+        long long int ans=0;
+        long long int s=0;
+        for(int i=0;i<k;i++){
+            s=s+a[i];
+        }
+        ans=s;
+        long long int maxi=s;
+        int i=0;
+        int j=k;
+        while(j<n){
+            s=s+a[j]-a[i];
+            maxi=max(s,maxi+a[j]);
+            ans=max(ans,maxi);
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};
