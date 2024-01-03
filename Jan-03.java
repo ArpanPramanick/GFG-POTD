a1e845a3225da50d70299e77dd024ce780018e8e
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-01-03 08:15:46 +0530

    Update Jan-03.java

--- Jan-03.java
+++ Jan-03.java
@@ -1,1 +1,21 @@
+//Link- https://www.geeksforgeeks.org/problems/smallest-window-containing-0-1-and-2--170637/1
 
+class Solution {
+    public int smallestSubstring(String S) {
+        // Code here
+        int zero=-1,one=-1,two=-1,res = 100001;
+
+        for(int i=0; i<S.length(); i++){
+            if(S.charAt(i)=='0') zero=i;
+            if(S.charAt(i)=='1') one=i;
+            if(S.charAt(i)=='2') two =i;
+
+            if(zero==-1 || one==-1 || two==-1) continue;
+            int min = Math.min(Math.min(one,zero),two);
+            int max = Math.max(Math.max(one,zero),two);
+            res = Math.min(res,max-min+1);
+
+        }
+        return res==100001?-1:res;
+    }
+};
