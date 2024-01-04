6b64e7e33579b86e2769ef33777be18f5336f80f
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-01-04 13:21:18 +0530

    Update Jan-04.cpp

--- Jan-04.cpp
+++ Jan-04.cpp
@@ -1,1 +1,19 @@
+//Link- https://www.geeksforgeeks.org/problems/find-element-occuring-once-when-all-other-are-present-thrice/1
 
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) {
+        // code here
+        sort(arr,arr+N);
+        vector<int>ans;
+        unordered_map<int,int>map;
+        for(int i =0;i<N;i++){
+            map[arr[i]]++;
+            if(map[arr[i]]==1)ans.push_back(arr[i]);
+            if(map[arr[i]]==2)ans.pop_back();
+        }
+    
+        return ans[0];
+    }
+    
+};
