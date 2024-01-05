6d9e1cffd723a1004b944e136da5f49aa93288a4
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-01-05 09:19:14 +0530

    Update Jan-05.cpp

--- Jan-05.cpp
+++ Jan-05.cpp
@@ -1,1 +1,16 @@
+//Link- https://www.geeksforgeeks.org/problems/count-possible-ways-to-construct-buildings5007/1
 
+class Solution{
+	public:
+	int TotalWays(int N)
+	{
+	    long long prev = 1;
+	    long long prevprev=1;
+	    const long long md = 1000 * 1000 * 1000LL + 7LL;
+	    for (int i=1; i<=N; ++i) {
+	        prevprev = (prev + prevprev) % md;
+	        std::swap(prev, prevprev);
+	    }
+	    return (prev * prev) % md;
+	}
+};
