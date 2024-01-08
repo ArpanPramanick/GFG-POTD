d39fe8e376845205f4af534cb196c4618c55908d
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-01-08 20:30:49 +0530

    Update Jan-08.cpp

--- Jan-08.cpp
+++ Jan-08.cpp
@@ -1,1 +1,56 @@
+//Link- https://www.geeksforgeeks.org/problems/merge-2-sorted-linked-list-in-reverse-order/1
 
+class Solution
+{
+    public:
+    
+    struct Node * mergeResult(Node *node1,Node *node2)
+    {
+        // your code goes here
+        Node *result = NULL;
+        while(node1!=NULL&&node2!=NULL)
+        {
+            
+            if(node1->data<=node2->data)
+            {
+                
+                Node *temp = node1->next;
+                node1->next = result;
+                result = node1;
+    
+                
+                node1=temp;
+            }else{
+             
+                Node *temp = node2->next;
+                node2->next=result;
+                result = node2;
+    
+                
+                node2=temp;
+            }
+        }
+    
+      
+    
+        while(node1!=NULL)
+        {
+           
+            Node *temp = node1->next;
+            node1->next = result;
+            result= node1;
+            node1 = temp;
+        }
+    
+        while(node2!=NULL)
+        {
+           
+            Node *temp = node2->next;
+            node2->next = result;
+            result = node2;
+            node2 = temp;
+        }
+     
+        return result;
+    }  
+};
