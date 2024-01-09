696519dc28cb0be055829d207e6117fbac997137
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-01-09 07:22:55 +0530

    Update Jan-09.cpp

--- Jan-09.cpp
+++ Jan-09.cpp
@@ -1,1 +1,16 @@
 
+//Link- https://www.geeksforgeeks.org/problems/search-pattern0205/1
+
+class Solution
+{public:
+        vector <int> search(string pat, string txt){
+            vector<int>ans;
+            int ind = txt.find(pat,0);
+            while(ind!=string::npos) {
+                ans.push_back(ind+1);
+                ind = txt.find(pat,ind+1);
+            }
+            return ans;
+        }
+     
+};
