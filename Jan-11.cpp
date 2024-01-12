48126a82db5c79ac2bfdbff6a20606460733aa91
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-01-12 09:35:43 +0530

    Update Jan-11.cpp

--- Jan-11.cpp
+++ Jan-11.cpp
@@ -1,1 +1,41 @@
+//Link- https://www.geeksforgeeks.org/problems/remove-k-digits/1
 
+// User function Template for C++
+
+class Solution {
+  public:
+    string removeKdigits(string S, int K) {
+        int n=S.size();
+        stack<char>st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty()&&st.top()>S[i]&&K>0)
+            {
+                st.pop();
+                K--;
+            }
+            if(st.empty()&&S[i]=='0')
+            continue;
+            st.push(S[i]);
+        }
+        while(!st.empty()&& K>0)
+        {
+           st.pop();
+           K--;
+        }
+
+       if(st.empty())
+       {
+       return "0";
+       }
+       string ans="";
+
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
