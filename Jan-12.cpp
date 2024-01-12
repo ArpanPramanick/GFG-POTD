048af6e96419761e9a6cc32bc08ad51d82dd9c26
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-01-12 09:34:12 +0530

    Update Jan-12.cpp

--- Jan-12.cpp
+++ Jan-12.cpp
@@ -1,1 +1,15 @@
 
+// User function Template for C++
+
+class Solution
+{
+    public:
+    
+    // Function to reverse first k elements of a queue.
+    queue<int> modifyQueue(queue<int> q, int k) {
+        stack<int > s; for(int i=0; i<k; i++) // inserting first k elements into the stack
+        { int x = q.front(); q.pop(); s.push(x); } while(!s.empty()) // Retriving the eleemnts from stack to queue 
+        { int x = s.top(); s.pop(); q.push(x); } int x = q.size()-k; while(x--) // IMP fetching the first (n-k) elements from the queue and push back into queue
+        { int val = q.front(); q.pop(); q.push(val); } return q;
+    }
+};
