0620b34296659a3b2c7791bfd3c5b990cd462590
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-02-18 08:23:31 +0530

    Update Feb-18.cpp

--- Feb-18.cpp
+++ Feb-18.cpp
@@ -1,1 +1,22 @@
+//Link- https://www.geeksforgeeks.org/problems/sum-of-leaf-nodes-in-bst/1
 
+class Solution
+{
+    public:
+        
+        int sumOfLeafNodes(Node *root){
+          
+            if (!root) {
+                return 0;
+            }
+        
+            
+            if (!root->left && !root->right){
+                return root->data;
+            }
+                
+        
+           
+            return sumOfLeafNodes(root->left) + sumOfLeafNodes(root->right);
+        }
+};
