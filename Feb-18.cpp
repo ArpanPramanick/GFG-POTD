1d9602d991c01ecb5f9afbdaa0f913982cf9bf81
Arpan Pramanick <86566396+ArpanPramanick@users.noreply.github.com>
2024-02-18 08:28:05 +0530

    Update Feb-18.cpp

--- Feb-18.cpp
+++ Feb-18.cpp
@@ -20,3 +20,19 @@ class Solution
             return sumOfLeafNodes(root->left) + sumOfLeafNodes(root->right);
         }
 };
+
+// -----------------------ARRAY VERSION-----------------------
+// int sum=0;
+//     for(int i=0;i<n;i++){
+//         int left=2*i+1, right=2*i+2;
+//         if(left<=n-1 || right<=n-1){
+//             if(arr[left]>=arr[i]) return -1;
+//             if(right<=n-1){
+//             if(arr[right]<arr[i]) return -1;
+//             }
+//         }
+//         else{
+//             sum+=arr[i];
+//         }
+//     }
+//     return sum;
